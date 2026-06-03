063915160f0ab1fa49fda60b88bd383c55eae071
shaaaaka <oliksandrfedorchuk@gmail.com>
2026-06-03 13:00:52 +0300

    Use %LocalAppData% in run_stealth.bat to support multiple users

--- run_stealth.bat
+++ run_stealth.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d "%~dp0"
-start "" "C:\Users\oliks\AppData\Local\Python\pythoncore-3.14-64\pythonw.exe" main.py
+start "" "%LocalAppData%\Python\pythoncore-3.14-64\pythonw.exe" main.py
 echo Screen Solver Bot has been started SILENTLY in the background.
 echo You can use your hotkey (Ctrl + Shift + F12) now.
 timeout /t 3
